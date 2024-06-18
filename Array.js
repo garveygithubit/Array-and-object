772a9f1a061b27272e36c8924c7e681958dd434b
Emmanuel <eamensah085@st.ug.edu.gh>
2024-06-18 16:43:53 +0000

    🚀 first commit

---
+++ Array.js
@@ -0,0 +1,21 @@
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+var mixed = ['ken', 4, 'chin-li', 8];
+mixed.push('ryu');
+mixed.push(1);
+mixed[0] = 3;
+//objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryn';
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
---
+++ Array.ts
@@ -0,0 +1,28 @@
+let names = ['luigi', 'mario','yoshi'];
+
+names.push('toad');
+
+
+let numbers = [10,20,30,40]
+numbers.push(25)
+
+let mixed = ['ken',4,'chin-li',8]
+mixed.push('ryu')
+mixed.push(1)
+mixed[0]=3
+
+//objects
+let ninja ={
+    name:   'mario',
+    belt:'black',
+    age:30
+}
+
+ninja.age=40
+ninja.name='ryn'
+
+ninja = { 
+    name:'yoshi',
+    belt:'orange',
+    age:40
+}
